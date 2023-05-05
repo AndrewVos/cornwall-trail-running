11d2aaf840b40373109b99f40b5489a8a391bbd3
Andrew Vos <andrew.vos@gmail.com>
2023-05-05 18:45:18 +0100

    Style the trail list

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,14 @@ import trails from "../data/trails"
 export default function Home() {
   return (
     <div className="space-y-3">
+      <h2 className="text-3xl font-bold text-gray-700">
+        Trails
+      </h2>
       {trails.map(trail => (
-        <div key={trail.name} className="border rounded p-3">
-          <Link className="text-blue-500 text-lg " href={`/trail/${trail.slug}`}>
-            <h3>
-              {trail.name}
-            </h3>
-          </Link>
+        <Link key={trail.name} className="block border rounded p-3 hover:bg-gray-50" href={`/trail/${trail.slug}`}>
+          <h3 className="text-lg font-bold text-pink-700 mb-3">
+            {trail.name}
+          </h3>
           <div className="text-lg text-gray-700">
             <span className="font-bold">Distance:</span> {trail.distance} km
           </div>
@@ -20,7 +21,7 @@ export default function Home() {
           <div className="text-lg text-gray-700">
             {trail.description}
           </div>
-        </div>
+        </Link>
       ))
       }
     </div >
