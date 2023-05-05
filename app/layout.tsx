3faf1aced0eea6b73987fea8344ee4d6f3e8be0b
Andrew Vos <andrew.vos@gmail.com>
2023-05-05 19:12:15 +0100

    Design tweaks

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,19 +11,44 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  const copyrightYear = () => {
+    const startYear = 2023
+    const currentYear = new Date().getFullYear()
+    return currentYear === startYear ? currentYear : `${startYear}-${currentYear}`
+  }
+
   return (
-    <html lang="en" className='h-full'>
-      <body>
-        <main className="container p-3 space-y-3">
-          <Link href="/">
-            <h1 className="text-4xl text-gray-700 font-bold hover:underline">
-              Cornwall Trail Running
-            </h1>
-          </Link>
+    <html lang="en">
+      <body className="container mx-auto space-y-3 p-3">
+        <header>
+          <div className=''>
+            <Link href="/">
+              <h1 className="text-4xl text-gray-700 font-bold hover:underline">
+                Cornwall Trail Running
+              </h1>
+            </Link>
+          </div>
+        </header>
+
+        <main className="">
           <div className="">
             {children}
           </div>
         </main>
+
+        <div className='border rounded px-3 py-1 flex items-center'>
+          <div className='text-sm text-gray-700 grow'>
+            Copyright Cornwall Trail Running {copyrightYear()}
+          </div>
+          <div>
+            <a className="text-blue-500 underline text-sm" href="mailto:andrew@andrewvos.com">Submit a GPX</a>
+          </div>
+          <div>&nbsp;-&nbsp;</div>
+          <div>
+            <a className="text-blue-500 underline text-sm" href="mailto:andrew@andrewvos.com">Contact</a>
+          </div>
+
+        </div>
 
       </body>
     </html>
